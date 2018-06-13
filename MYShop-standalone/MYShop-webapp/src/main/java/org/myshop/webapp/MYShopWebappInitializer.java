5d3aaafabaf5e0fc40ced0591f6423dd318086f8
elabrea <estifen.abrea@gmail.com>
2018-06-13 11:04:45 +0800

    Additional for startUP 'Hello World'

--- MYShop-standalone/MYShop-webapp/src/main/java/org/myshop/webapp/MYShopWebappInitializer.java
+++ MYShop-standalone/MYShop-webapp/src/main/java/org/myshop/webapp/MYShopWebappInitializer.java
@@ -1,4 +1,21 @@
 package org.myshop.webapp;
 
-public class MYShopWebappInitializer {
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class MYShopWebappInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {WebConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
 }
--- MYShop-standalone/MYShop-webapp/src/main/java/org/myshop/webapp/WebConfig.java
+++ MYShop-standalone/MYShop-webapp/src/main/java/org/myshop/webapp/WebConfig.java
@@ -1,4 +1,30 @@
 package org.myshop.webapp;
 
-public class WebConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "org.myshop.webapp")
+public class WebConfig extends WebMvcConfigurerAdapter{
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("/js");
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver =
+                new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        resolver.setExposeContextBeansAsAttributes(true);
+        return resolver;
+    }
 }
